f24dc075f88fc959ce67a42752a23e216304d840
Amit Samra <asamra@zipcodes-MacBook-Pro.local>
2020-03-16 22:14:15 -0400

    Finished missing portions

---
+++ json_helper2.py
@@ -0,0 +1,92 @@
+# Exercise 1
+
+import os
+import json
+import pickle
+
+path = "/Users/asamra/dev/PythonFundamentals.Exercises.Part9/data/super_smash_bros/link.json"
+path2 = "/Users/asamra/dev/PythonFundamentals.Exercises.Part9/data/super_smash_bros/"
+
+
+# Part A
+def read_json(file_path):
+	with open(file_path) as f:
+		json_input = json.load(f)
+	return json_input
+
+print(read_json(path))
+
+"""
+In Part A, we are using the OS Module to open and read a JSON file and then 
+return a Python dictionary. Meaning, the string contents of a JSON file are
+being deserialized into a usable Python data structure, a dictionary. 
+"""
+
+# Part B
+def read_all_json_files(file_path):
+	for direpath, direnames, filenames in os.walk(file_path):
+		result = []
+		for f in filenames:
+			if f.endswith('.json'):
+				json_content = read_json(os.path.join(file_path,f))
+				result.append(json_content)
+	return result
+
+print(read_all_json_files(path2))
+
+"""
+In Part B, we want to apply the function from Part A to all files in a particular
+directory. 
+
+We begin by declaring 3 arguments required for the os.walk() method: root, _, and files.
+
+In the official documentation, they are called: direpath, direnames, filenames. 
+https://docs.python.org/3/library/os.html 
+Note how the direnames was renamed _ since it is not relevant here. 
+
+dirpath (root) is a string, the path to the directory. 
+dirnames is a list of the names of the subdirectories in dirpath (excluding '.' and '..'). 
+filenames is a list of the names of the non-directory files in dirpath. 
+Note that the names in the lists contain no path components. To get a full path 
+(which begins with top) to a file or directory in dirpath, do os.path.join(dirpath, name).
+
+os.walk() generates the file names in a directory tree by walking the tree either top-down or bottom-up. 
+For each directory in the tree rooted at directory top (including top itself), it yields a 3-tuple (dirpath, dirnames, filenames).
+
+Then, we create an empty list and add files to the list if the file_path + f ends with .json,
+meaning that it's a JSON file. 
+
+"""
+
+
+# Part C
+
+def write_pickle(file_path, data):
+	with open(file_path, 'wb') as handler:
+		pickle.dump(data, handler)
+
+
+# Part D
+
+def load_pickle(file_path):
+	with open(file_path, 'rb') as handler:
+		data = pickle.load(handler)
+	return data
+
+"""
+Pickle reads (Part C) / writes (Part D) the python object as binary. It preserves the object as it was in memory.
+"""
+
+
+# Exercise 2
+
+path3 = "/Users/asamra/dev/PythonFundamentals.Exercises.Part9/data/marvel/marvel.json"
+
+content = read_all_json_files(path3)
+print(content)
+
+write_pickle(path3, content)
+marvel_content = load_pickle(path3)
+print(marvel_content)
+
+
